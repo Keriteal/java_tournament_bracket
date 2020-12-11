3893bbc912e53c9101ad46a15897f0967ffb522e
547036318 <547036318@qq.com>
2020-12-11 23:18:57 +0800

    fixed httpStatus

--- src/main/java/com/tournament/managerment/exception/user/LoginErrorException.java
+++ src/main/java/com/tournament/managerment/exception/user/LoginErrorException.java
@@ -7,6 +7,6 @@ public class LoginErrorException extends BaseException {
     private static final long serialVersionUID = -119827672287342330L;
 
     public LoginErrorException(String userName){
-        super(HttpStatus.ACCEPTED, "login failure: " + userName);
+        super(HttpStatus.BAD_REQUEST, "login failure: " + userName);
     }
 }
