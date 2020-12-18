cf4a110b4b64662cab83e944e6eea7ca526293d6
Keriteal <keriteal985@gmail.com>
2020-12-18 20:08:37 +0800

    badrequest to unauthorized if not logged in

--- src/main/java/com/tournament/managerment/exception/tournament/InvalidUserHeaderException.java
+++ src/main/java/com/tournament/managerment/exception/tournament/InvalidUserHeaderException.java
@@ -3,11 +3,11 @@ package com.tournament.managerment.exception.tournament;
 import com.tournament.managerment.exception.BaseException;
 import org.springframework.http.HttpStatus;
 
-public class MissingUserException extends BaseException {
+public class InvalidUserHeaderException extends BaseException {
 
     private static final long serialVersionUID = -1909654886770732320L;
 
-    public  MissingUserException() {
-        super(HttpStatus.BAD_REQUEST, "missing User-Name in HTTP Header");
+    public InvalidUserHeaderException() {
+        super(HttpStatus.UNAUTHORIZED, "missing User-Name in HTTP Header");
     }
 }
